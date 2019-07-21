6d927b93229cd16258c0506e09b647fcd61e87db
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-21 22:17:40 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,2 @@
-puts (meal_choice("brocolli","macaroni")).to eq("A plate of meat with broccoli and macaroni.")
+expect (meal_choice("brocolli","macaroni")).to eq("A plate of meat with broccoli and macaroni.")
+expect (meal_choice("broccoli",macaroni",tofu")).to eq("A plate of tofu with broccoli and macaroni.")
